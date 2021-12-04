da7e887c520f32be0302c44ac2fe1492c419d77a
azad ansari <azadansari7277@gmail.com>
2021-12-04 20:54:55 +0530

    classes, rest and spread

---
+++ Module 1/classes.js
@@ -0,0 +1,13 @@
+class Employee{
+
+    constructor(name, dept){
+        this.name = name;
+        this.dept = dept;
+    }
+
+    print(){
+        console.log(this.name+" Dept: "+this.dept);
+    }
+}
+const emp = new Employee("John", "IT");
+emp.print();
