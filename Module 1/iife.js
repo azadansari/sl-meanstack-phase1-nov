0ee14cd568626e3c679f7eeeafd608ddf03b8ba6
azad ansari <azadansari7277@gmail.com>
2021-12-16 20:55:47 +0530

    iife learn more about

--- Module 1/iife.js
+++ Module 1/iife.js
@@ -1,6 +1,19 @@
 
-const printFunction = function(){
-    console.log("Printing...");
-}();
+// const printFunction = function(){
+//     console.log("Printing...");
+// }();
 
-printFunction;
+// printFunction;
+
+const example = (function(){
+    count = 0;
+    return function(){
+        ++count;
+        return `emp${count}`;
+    };
+});
+console.log(example());
+// console.log('emp01:' +example());
+// console.log('emp02:' +example());
+// console.log('emp03:' +example());
+// console.log('emp04:' +example());
